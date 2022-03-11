c77481a60f136646774b163473ad43dd9a94641d
fgr <f.gerard@evs.com>
2022-03-11 11:47:11 +0100

    fix googletest branch name

--- cmake/googletest-download.cmake
+++ cmake/googletest-download.cmake
@@ -18,7 +18,7 @@ include(ExternalProject)
 
 ExternalProject_Add( googletest
    GIT_REPOSITORY https://github.com/google/googletest.git
-   GIT_TAG master
+   GIT_TAG main
    SOURCE_DIR "${CMAKE_BINARY_DIR}/googletest-src"
    BINARY_DIR "${CMAKE_BINARY_DIR}/googletest-build"
    CMAKE_CACHE_ARGS -DCMAKE_C_COMPILER:FILEPATH="${CMAKE_C_COMPILER}"
